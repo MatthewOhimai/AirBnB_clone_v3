437199565a79a1947805c7ec75028d62c2f994ab
blessi30ng <blessingchidewu3@gmail.com>
2024-08-06 09:44:15 +0000

    lol

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-
-
-from models import storage
-from models import *
-from flask import Flask
-from api.v1.views import app_views
-app = Flask(__name__)
-=======
 """
 app
 """
+
 
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -24,25 +16,13 @@ app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
->>>>>>> ca499d5931e9c613c9f849d99b53b85a705dff64
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-<<<<<<< HEAD
 	"""closes the storage"""
 	storage.close()
-
-
-if __name__ == '__main__':
-	app.run(host='HBNB_API_HOST', port='HBNB_API_PORT', threaded=True)
-	
-=======
-    """
-    teardown function
-    """
-    storage.close()
 
 
 @app.errorhandler(404)
@@ -60,6 +40,6 @@ def handle_404(exception):
 
     return(resp)
 
+
 if __name__ == "__main__":
     app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
->>>>>>> ca499d5931e9c613c9f849d99b53b85a705dff64
